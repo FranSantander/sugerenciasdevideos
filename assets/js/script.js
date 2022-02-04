de05677d837f96f42a6ac9f3d9ff401e114e7768
FranSantander <naturatiendacopiapo@gmail.com>
2022-02-04 00:41:38 -0300

    tercero subiendo modificacion script

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,12 @@
 //new Reproductor ("link youtube", peliculas)
 //play musica.PlayMultimedia
 //play.setInicio(100)
-let Myapp = (()=>{
-    let parametros = (url, id) =>{
-        id.setAtributte("src", url);
-        id.style.display= "block";
+const patronAutoEjecutable = (() => {
+    const data = (url, id) => {
+        id.setAtributte('src', url);
+        //id.style.display= "block";
     };
-    return{mostrarTodo: (url, id)=> parametros(url,id)}
+    return{mostrarInfo: (url, id)=> data (url,id)}
 })();
 
 class Multimedia {
@@ -19,40 +19,43 @@ class Multimedia {
         this.getUrl = () => _url;
     }
      getUrl(){
-        return this._getUrl();
+        return this.getUrl();
     }
-    setInicio = () =>{
+    setInicio = () => {
         return `Este método es para realizar un cambio en la URL del video`
     }
 }
+
 class Reproductor extends Multimedia{
     constructor(url, id){
         super(url);
         let _id = id;
-        this._getId = () => _id;
+        this.getId = () => _id;
     }
-   playMultimedia = () =>{
-        Myapp.mostrarTodo(this.url, this.getId())
+   playMultimedia = () => {
+    patronAutoEjecutable.mostrarInfo(this.url, this.getId());
    } 
    setInicio(tiempo){
-       this.getId().setAtributte("src", `${this.url}?start=${tiempo}`)
+       this.getId().setAtributte('src', `${this.url}?start=${tiempo}`)
    }  
 }
 
 let music1 = new Reproductor('https://www.youtube.com/embed/bu7nU9Mhpyo', musica);
-music1.playMultimedia();
-music1.setInicio(65);
 
 let peli1 = new Reproductor('https://www.youtube.com/embed/918rOI1gnQ8', peliculas);
-peli1.playMultimedia();
-peli1.setInicio(1900);
-
 
 let serie1 = new Reproductor('https://www.youtube.com/embed/H2HjT1yvin8', series);
+
+
+
+
+music1.playMultimedia();
+peli1.playMultimedia();
 serie1.playMultimedia();
+
+music1.setInicio(65);
+peli1.setInicio(1900);
 serie1.setInicio(321);
-
-
 //seleccionMedios.addEventListener("click", function(){
   //  console.log(seleccionMedios.value);
 //})
