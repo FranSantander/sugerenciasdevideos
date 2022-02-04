53517a3b7d8ea091e2f7894d27c22c3d92fe4044
FranSantander <naturatiendacopiapo@gmail.com>
2022-02-04 01:12:35 -0300

    cuarto subiendo desafio terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,85 @@
-//primero crear funcion autoejecutable: pasar por parametros la url e id 
-//segundo realizar la clase multimedia y reproductor (extends de multimedia)
-//tercero instancia de musica
-//llamar método para ejecutar la música: 
-//new Reproductor ("link youtube", peliculas)
-//play musica.PlayMultimedia
-//play.setInicio(100)
+// Función ejecutadas inmediatamente (IIFE)
+// **Metodo setAttribute, que toma los dos parametros.
+// En donde el primero es el nombre de la propiedad a setear y el segundo es el valor de dicha propiedad
+
 const patronAutoEjecutable = (() => {
+    //Variable  para el video
     const data = (url, id) => {
-        id.setAtributte('src', url);
-        //id.style.display= "block";
+      id.setAttribute('src', url);
+     };
+  
+    //Retornado data
+    return {
+      mostrarInfo: (url, id) => data(url, id),
     };
-    return{mostrarInfo: (url, id)=> data (url,id)}
-})();
-
-class Multimedia {
-    constructor(url){
-        let _url = url;
-        this.getUrl = () => _url;
+  })();
+  
+  //Creando las clases aplicando Closure ES6
+  class Multimedia {
+    constructor(url) {
+      //  Atributo privado (_)
+      let _url = url;
+  
+      // Funcion interna
+      this.getUrl = () => _url;
     }
-     getUrl(){
-        return this.getUrl();
+  
+    //Agregando  método Get
+    get url() {
+      return this.getUrl();
     }
-    setInicio = () => {
-        return `Este método es para realizar un cambio en la URL del video`
+    //Metodo que retorb solo un mensaje
+    setInicio() {
+      return 'Este método es para realizar un cambio en la URL del video';
     }
-}
-
-class Reproductor extends Multimedia{
-    constructor(url, id){
-        super(url);
-        let _id = id;
-        this.getId = () => _id;
+  }
+  
+  //Creando las clases aplicando Closure ES6
+  // **Clase Hija(o) de Multimedia
+  
+  class Reproductor extends Multimedia {
+    constructor(url, id) {
+      super(url);
+      //  Atributo privado (_)
+      let _id = id;
+      // Funcion interna
+      this.getId = () => _id;
+  
+      //Metodo que llama a  la Función Pública de la IIFE (url,id)
     }
-   playMultimedia = () => {
-    patronAutoEjecutable.mostrarInfo(this.url, this.getId());
-   } 
-   setInicio(tiempo){
-       this.getId().setAtributte('src', `${this.url}?start=${tiempo}`)
-   }  
-}
-
-let music1 = new Reproductor('https://www.youtube.com/embed/bu7nU9Mhpyo', musica);
-
-let peli1 = new Reproductor('https://www.youtube.com/embed/918rOI1gnQ8', peliculas);
-
-let serie1 = new Reproductor('https://www.youtube.com/embed/H2HjT1yvin8', series);
-
-
-
-
-music1.playMultimedia();
-peli1.playMultimedia();
-serie1.playMultimedia();
-
-music1.setInicio(65);
-peli1.setInicio(1900);
-serie1.setInicio(321);
-//seleccionMedios.addEventListener("click", function(){
-  //  console.log(seleccionMedios.value);
-//})
+    playMultimedia() {
+      //IIFE.variable_que_contenga_informacion_a_Mostrar
+      patronAutoEjecutable.mostrarInfo(this.url, this.getId());
+    }
+  
+    //Metodo para setear tiempo de inicio de la URl con setAttribute
+    setInicio(tiempo) {
+      this.getId().setAttribute('src', `${this.url}?start=${tiempo}`);
+    }
+  }
+  
+  //Instanciando Muscia, Video y Serie de la clase Reprocuctor
+  //Dando el argumento la url del video y y el id(del html)
+  
+  let musicaVideo = new Reproductor(
+    'https://www.youtube.com/embed/LrCzjsSauVQ',
+    musica
+  );
+  let peliculasVideo = new Reproductor(
+    'https://www.youtube.com/embed/918rOI1gnQ8',
+    peliculas
+  );
+  let seriesVideo = new Reproductor(
+    'https://www.youtube.com/embed/oXn6UBUUycU',
+    series
+  );
+  
+  //Invocar metodo de la clase
+  
+  musicaVideo.playMultimedia();
+  peliculasVideo.playMultimedia();
+  seriesVideo.playMultimedia();
+  
+  musicaVideo.setInicio(102);
+  peliculasVideo.setInicio(1902);
+  seriesVideo.setInicio(322);
